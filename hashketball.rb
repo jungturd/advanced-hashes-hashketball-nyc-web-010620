f7329bdbef8dca2b11b9195b009f61913a0042c1
Jeemin Shim <jeemin.shim@gmail.com>
2020-01-03 17:39:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,57 +1,7 @@
 require 'pry'
 
 def game_hash
-    home: { team_name: 'Brooklyn Nets',
-            colors: %w[Black White],
-            players: [
-              { player_name: 'Alan Anderson',
-                number: 0,
-                shoe: 16,
-                points: 22,
-                rebounds: 12,
-                assists: 12,
-                steals: 3,
-                blocks: 1,
-                slam_dunks: 1 },
-              { player_name: 'Reggie Evans',
-                number: 30,
-                shoe: 14,
-                points: 12,
-                rebounds: 12,
-                assists: 12,
-                steals: 12,
-                blocks: 12,
-                slam_dunks: 7 },
-              { player_name: 'Brook Lopez',
-                number: 11,
-                shoe: 17,
-                points: 17,
-                rebounds: 19,
-                assists: 10,
-                steals: 3,
-                blocks: 1,
-                slam_dunks: 15 },
-              { player_name: 'Mason Plumlee',
-                number: 1,
-                shoe: 19,
-                points: 26,
-                rebounds: 11,
-                assists: 6,
-                steals: 3,
-                blocks: 8,
-                slam_dunks: 5 },
-              { player_name: 'Jason Terry',
-                number: 31,
-                shoe: 15,
-                points: 19,
-                rebounds: 2,
-                assists: 2,
-                steals: 4,
-                blocks: 11,
-                slam_dunks: 1 }
-            ] },
-            
-              {
+  {
     away: { team_name: 'Charlotte Hornets',
             colors: %w[Turquoise Purple],
             players: [
@@ -100,6 +50,55 @@ def game_hash
                 steals: 7,
                 blocks: 5,
                 slam_dunks: 12 }
+            ] },
+    home: { team_name: 'Brooklyn Nets',
+            colors: %w[Black White],
+            players: [
+              { player_name: 'Alan Anderson',
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+                steals: 3,
+                blocks: 1,
+                slam_dunks: 1 },
+              { player_name: 'Reggie Evans',
+                number: 30,
+                shoe: 14,
+                points: 12,
+                rebounds: 12,
+                assists: 12,
+                steals: 12,
+                blocks: 12,
+                slam_dunks: 7 },
+              { player_name: 'Brook Lopez',
+                number: 11,
+                shoe: 17,
+                points: 17,
+                rebounds: 19,
+                assists: 10,
+                steals: 3,
+                blocks: 1,
+                slam_dunks: 15 },
+              { player_name: 'Mason Plumlee',
+                number: 1,
+                shoe: 19,
+                points: 26,
+                rebounds: 11,
+                assists: 6,
+                steals: 3,
+                blocks: 8,
+                slam_dunks: 5 },
+              { player_name: 'Jason Terry',
+                number: 31,
+                shoe: 15,
+                points: 19,
+                rebounds: 2,
+                assists: 2,
+                steals: 4,
+                blocks: 11,
+                slam_dunks: 1 }
             ] }
   }
 end
@@ -188,4 +187,63 @@ def big_shoe_rebounds
   end
 
   num_rebounds
+end
+
+# # Bonus Questions
+
+# # Since there are multiple bonus questions that ask me
+# # to return the name of a player with the most of some stat, I can use the following methods
+# # to DRY (don't repeat yourself) up my code.
+
+def iterate_through_players_for(name, statistic)
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      return player[statistic] if player[:player_name] == name
+    end
+  end
+end
+
+def player_with_most_of(statistic)
+  player_name = nil
+  amount_of_stat = 0
+
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      if player[statistic].is_a? String
+        if player[statistic].length > amount_of_stat
+          amount_of_stat = player[statistic].length
+          player_name = player[:player_name]
+        end
+      elsif player[statistic] > amount_of_stat
+        amount_of_stat = player[statistic]
+        player_name = player[:player_name]
+      end
+    end
+  end
+
+  player_name
+end
+
+def most_points_scored
+  player_with_most_of(:points)
+end
+
+def winning_team
+  # Set up a hash to keep track of the points scored by each team. This way, we
+  # can iterate through each player, get their points scored, and increase the
+  # count in the hash.
+
+  scores = { 'Brooklyn Nets' => 0, 'Charlotte Hornets' => 0 }
+
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      scores[game_data[:team_name]] += iterate_through_players_for(player[:player_name], :points)
+    end
+  end
+
+  scores.max_by { |_k, v| v }.first
+end
+
+def player_with_longest_name
+  player_with_most_of(:player_name)
 end
